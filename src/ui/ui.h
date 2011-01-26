ed54151b4bcdd5031c34d99b984fb1669edc681d
swiftos <swiftos@lavabit.com>
2011-01-26 16:50:51 +0100

    Yeah man

--- src/ui/ui.h
+++ src/ui/ui.h
@@ -1,4 +1,12 @@
 /* User interface, list mails, display operations in progress, etc
  */
 
-/* vim:set shiftwidth=4: */
+void do_resize(int sig);
+int init_curses();
+int end_curses();
+void update_ui();
+void draw_windows();
+WINDOW* create_window(int height, int width, int starty, int startx);
+WINDOW* create_menu_window(int height, int width, int starty, int startx);
+WINDOW* create_status_window(int height, int width, int starty, int startx);
+void del_window(WINDOW* win);
